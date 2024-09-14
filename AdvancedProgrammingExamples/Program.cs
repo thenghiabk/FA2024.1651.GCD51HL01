0db3c0246fececb2950da95e8d0ce6509ff91c8e
thenghiabk <thenghiabk@gmail.com>
2024-09-14 10:25:40 +0700

    first commit

---
+++ AdvancedProgrammingExamples/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+
+namespace AdvancedProgrammingExamples
+{
+    
+    class UndergraduteStudent
+    {
+        // data
+        private string name;
+        private int id;
+        private string address;
+        private int age;
+
+        // properties
+        public int Age
+        {
+            get => age;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                age = value;
+            }
+        }
+        public string Name { 
+            get { return name; } 
+            private set { name = value; }
+        }
+
+        public int Id
+        {
+            get { return id; }
+            private set { id = value; }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { 
+                address = value.ToUpper(); 
+            }
+        }
+
+        // constuctor
+        public UndergraduteStudent()
+        {
+            this.name = "Unknown";
+        }
+
+        public UndergraduteStudent(string name)
+        {
+            this.name = name;
+        }
+
+        // methods
+        
+    }
+
+    class GraduateStudent : UndergraduteStudent
+    {
+        // data
+        private string researchTopic;
+
+        // properties
+        public string ResearchTopic
+        {
+            get { return researchTopic; } 
+            private set { researchTopic = value; }
+        }
+
+        // constuctor
+        public GraduateStudent() : base()
+        {
+            
+        }
+
+        public GraduateStudent(string name, string researchTopic) : base (name) // 1
+        {
+            
+           
+            // 2
+            this.researchTopic = researchTopic;
+        }
+
+        // methods
+
+    }
+
+    internal class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            UndergraduteStudent student1 = new UndergraduteStudent("John");
+            GraduateStudent student2 = new GraduateStudent("David", "IoT");
+
+            Console.WriteLine(student1.Name);
+            Console.WriteLine(student2.Name);
+
+
+
+        }
+    }
+}
---
+++ UML_ClassDiagram_Association/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UML_ClassDiagram_Association
+{
+    class Doctor
+    {
+        public string Name { get; private set; }
+        public string Department { get; private set; }
+        public Doctor(string name, string department)
+        {
+            Name = name;
+            Department = department;
+        }
+        public void CheckUp(Patient patient)
+        {
+            Console.WriteLine($"The doctor `{this.Name}` is checking up for the patient `{patient.Name}`");
+        }
+    }
+
+    class Patient
+    {
+        public string Name { get; private set; }
+        public string Address { get; private set; }
+        public Patient(string name, string address)
+        {
+            Name = name;
+            Address = address;
+        }
+        public void GoToCheckUp(Doctor doctor)
+        {
+            Console.WriteLine($"The patient `{this.Name}` is checked up by the doctor `{doctor.Name}`");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Doctor doctor = new Doctor("John Doe", "Plastic Surgery");
+            Patient patient = new Patient("Nancy", "658 Ngo Quyen");
+            doctor.CheckUp(patient);
+            patient.GoToCheckUp(doctor);
+        }
+    }
+}
